e1e1863cbb740bf9a178b1b5c691f250d631457e
subieremg <subieremarygraceit1b@gmail.com>
2017-03-19 17:52:15 +0800

    first

--- server.js
+++ server.js
@@ -8,13 +8,25 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/app.html'));
+app.get('/', function(req, res){ 
+  res.sendFile(path.join(__dirname, 'views/Welcome.html'));
 });
-app.get('/about', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/about.html'));
+app.get('/Twilight', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/Twilight.html'));
 });
-app.get('*', function(req, res){
+app.get('/Newmoon', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/NewMoon.html'));
+  });
+app.get('/Eclipse', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/Eclipse.html'));
+  });
+app.get('/BDP1', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/BDP1.html'));
+  });
+app.get('/BDP2', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/BDP2.html'));
+  });
+app.get('*', function(req, res){ 
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
 //express server listen
